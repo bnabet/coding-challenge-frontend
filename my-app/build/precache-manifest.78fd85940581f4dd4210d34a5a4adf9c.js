e63c783ac8865f6634744a034ca1d9ffa4ba37e0
Benjamin Nabet <ben.nabet08@gmail.com>
2020-04-24 15:17:40 +0200

    Build

---
+++ my-app/build/precache-manifest.78fd85940581f4dd4210d34a5a4adf9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7272bb87759cdd9a8cf9b4993f2da3e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7aea961bd9bfa6eee82c",
+    "url": "/static/css/main.efd1441f.chunk.css"
+  },
+  {
+    "revision": "d38db5b14b8e9e069c4f",
+    "url": "/static/js/2.921c2828.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.921c2828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aea961bd9bfa6eee82c",
+    "url": "/static/js/main.30995baf.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
